d388e346041b07b0f6fefa5c5ea737985a185a87
mvenkat6 <maitreya_venkataswamy@brown.edu>
2020-11-21 12:37:24 -0500

    Added sample images and implemented weather data downloader

--- data_acquire.py
+++ data_acquire.py
@@ -6,13 +6,17 @@ __author__ = "Maitreya Venkataswamy"
 
 import requests
 import pandas as pd
-from datetime import timedelta
+from datetime import timedelta, datetime
+from functools import lru_cache
+from netCDF4 import Dataset
 
 
 TURBINE_DATA_URL = "https://eersc.usgs.gov/api/uswtdb/v1/turbines" + \
                    "?&t_state=in.(WA,OR)&select=t_cap,t_hh,xlong,ylat"
 
 POWER_DATA_URL = "https://transmission.bpa.gov/Business/Operations/Wind/twndbspt.txt"
+
+WEATHER_DATA_URL = "https://nomads.ncep.noaa.gov/dods/gfs_0p25_1hr/gfs{}/gfs_0p25_1hr_{:02d}z"
 
 
 def download_turbine_data():
@@ -45,6 +49,19 @@ def download_power_data():
     return power_data
 
 
+@lru_cache(maxsize=16)
+def download_weather_data(date, hr_base=0):
+    """Downloads a single weather forecast dataset from the GFS system"""
+    # Get the date as a string formatted as YYYYmmdd
+    date_str = date.strftime("%Y%m%d")
+
+    # Download and return the dataset
+    try:
+        return Dataset(WEATHER_DATA_URL.format(date_str, hr_base))
+    except OSError:
+        return None
+
+
 class DataAcquirer:
     """A class for a data acquisition program"""
 
@@ -60,6 +77,8 @@ class DataAcquirer:
 def main():
     """Main program execution"""
     data_acquirer = DataAcquirer(turbine_data_file="turbine_metadata.csv")
+
+    download_weather_data(datetime.today().date())
 
 
 if __name__ == "__main__":
